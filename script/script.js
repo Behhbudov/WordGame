5d43fa287640b30849578e8a9a2ebd543f8fc5ef
Murad Behbudov <murad.5behbudov@yahoo.com>
2023-12-11 20:43:04 +0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,81 @@
+const wordText = document.querySelector(".word"),
+  hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
+  inputField = document.querySelector("input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
+closeBtn = document.querySelector(".close-btn");
+backdrop = document.querySelector(".backdrop");
+modalContent = document.querySelector(".modal-content");
+
+let correctWord, timer;
+
+const initTimer = (maxTime) => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      if (inputField.value == "" && maxTime == 0) {
+        backdrop.style.visibility = "visible";
+        modalContent.innerText = "Vaxt bitdi. Uduzdun!!!";
+        clearInterval(timer);
+        return initGame();
+      }
+      maxTime--;
+      return (timeText.innerText = maxTime);
+    }
+    if (inputField.value == "" && maxTime == 0) {
+      backdrop.style.visibility = "visible";
+      modalContent.innerText = "Vaxt bitdi. Uduzdun!!!";
+      clearInterval(timer);
+      return initGame();
+    }
+    // backdrop.style.visibility = "visible";
+    // modalContent.innerText = `Vaxt bitdi! ${correctWord.toUpperCase()} was the correct word`;
+    initGame();
+  }, 1000);
+};
+
+const initGame = () => {
+  // backdrop.style.visibility = "visible";
+  initTimer(30);
+  let randomObj = words[Math.floor(Math.random() * words.length)]; // Getting random object from words array
+  let wordArray = randomObj.word.split(""); // Splitting the word
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+  }
+  wordText.innerText = wordArray.join("");
+  hintText.innerText = randomObj.hint;
+  correctWord = randomObj.word;
+  inputField.value = "";
+  inputField.setAttribute("maxLength", correctWord.length);
+};
+initGame();
+
+const checkWord = () => {
+  let userWord = inputField.value.toLowerCase();
+  if (!userWord) {
+    backdrop.style.visibility = "visible";
+    modalContent.innerText = "Yoxlamaq istədiyin sözü daxil et!";
+    // return alert("Please enter the word to check!");
+  }
+  if (userWord !== correctWord) {
+    backdrop.style.visibility = "visible";
+    modalContent.innerText = `Oops! ${userWord.toUpperCase()} doğru söz deyil`;
+    return;
+    // return alert(`Oops! ${userWord.toUpperCase()} is not a correct word`);
+  } else {
+    backdrop.style.visibility = "visible";
+    modalContent.innerText = `Təbriklər! <b>${userWord.toUpperCase()}</b> doğru sözdür`;
+    clearInterval(timer);
+    // alert(`Congrats! ${userWord.toUpperCase()} is the correct word`);
+  }
+  initGame();
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
+
+closeBtn.addEventListener("click", () => {
+  backdrop.style.visibility = "hidden";
+});
---
+++ script/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "toplama",
+        hint: "Rəqəmlərin əlavə edilməsi prosesi"
+    },
+    {
+        word: "iclas",
+        hint: "İnsanların bir araya gəldiyi hadisə"
+    },
+    {
+        word: "nömrə",
+        hint: "Saymaq üçün istifadə olunan riyaziyyat simvolu"
+    },
+    {
+        word: "mübadilə",
+        hint: "bir şeyi vermək və əvəzində başqa bir şey almaq"
+    },
+    {
+        word: "kətan",
+        hint: "Yağlı boya üçün əski parçası"
+    },
+    {
+        word: "bağ",
+        hint: "Çiçək və bitki əkmək üçün yer"
+    },
+    {
+        word: "mövqe",
+        hint: "Kiminsə və ya bir şeyin yeri"
+    },
+    {
+        word: "lələk",
+        hint: "Tükü quşun xarici örtüyünə bənzər"
+    },
+    {
+        word: "rahatlıq",
+        hint: "Xoş bir istirahət hissi"
+    },
+    {
+        word: "dil",
+        hint: "Ağızın əzələ orqanı"
+    },
+    {
+        word: "genişlənmə",
+        hint: "Artma və ya böyümə prosesi"
+    },
+    {
+        word: "ölkə",
+        hint: "Siyasi olaraq müəyyən edilmiş bir bölgə"
+    },
+    {
+        word: "qrup",
+        hint: "Bir sıra obyektlər və ya şəxslər"
+    },
+    {
+        word: "dad",
+        hint: "Dilin ləzzəti aşkar etmək qabiliyyəti"
+    },
+    {
+        word: "mağaza",
+        hint: "Malların satıldığı böyük mağaza"
+    },
+    {
+        word: "tarla",
+        hint: "Təsərrüfat fəaliyyəti üçün torpaq sahəsi"
+    },
+    {
+        word: "dost",
+        hint: "Ailə üzvündən başqa şəxs"
+    },
+    {
+        word: "cib",
+        hint: "Kiçik əşyaların daşınması üçün çanta"
+    },
+    {
+        word: "iynə",
+        hint: "İncə və kəskin metal sancaq"
+    },
+    {
+        word: "ekspert",
+        hint: "Geniş biliyə malik insan"
+    },
+    {
+        word: "bəyanat",
+        hint: "Bir şeyin bəyanı"
+    },
+    {
+        word: "saniyə",
+        hint: "Dəqiqənin altmışda biri"
+    },
+    {
+        word: "kitabxana",
+        hint: "Kitab kolleksiyası olan yer"
+    },
+]
